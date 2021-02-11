a736737be5b036dc9e8f15d6d70b97f78fe7e767
Santhosh136 <snthosh602@gmail.com>
2021-02-11 19:35:42 +0530

    add init files

---
+++ student-forum-app/src/main/java/info/javacafe/studentforumapp/StudentForumAppApplication.java
@@ -0,0 +1,13 @@
+package info.javacafe.studentforumapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentForumAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentForumAppApplication.class, args);
+	}
+
+}
---
+++ student-forum-app/src/test/java/info/javacafe/studentforumapp/StudentForumAppApplicationTests.java
@@ -0,0 +1,13 @@
+package info.javacafe.studentforumapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentForumAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
